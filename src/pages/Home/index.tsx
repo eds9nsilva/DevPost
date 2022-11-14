da40660435d8c69340543e459b16845ff70e60f1
eds9nsilva <antonioedson99@gmail.com>
2022-11-14 15:47:48 -0300

    create page and routes new post

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
+import {Container, ButtonPost} from './styles';
+import Feather from 'react-native-vector-icons/Feather';
+import {useNavigation} from '@react-navigation/native'
 
-function Home(){
-  return(
-    <View>
-      <Text>TELA HOME</Text>
-    </View>
-  )
+function Home() {
+  const navigation = useNavigation()
+  return (
+    <Container>
+      <ButtonPost onPress={() => navigation.navigate("NewPost")} activeOpacity={0.8}>
+        <Feather name="edit-2" color="#fff" size={25} />
+      </ButtonPost>
+    </Container>
+  );
 }
 
 export default Home;
